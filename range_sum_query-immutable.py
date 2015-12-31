a541201faf74f9835ab91030a183d7e6f5a2f955
ksang <kaitoy@qq.com>
2015-12-31 16:45:55 +0800

    range sum query

--- range_sum_query-immutable.py
+++ range_sum_query-immutable.py
@@ -4,19 +4,14 @@ class NumArray(object):
         initialize your data structure here.
         :type nums: List[int]
         """
-        self.nums = nums
-        self.interval = []
-        m = 0
-        while 15 + m*15 < len(nums):
-            self.interval.append(self.calcRange(0+m*15, 15+m*15))
-            m += 1
-
-    def calcRange(self, i, j):
-        res = 0
-        while i <= j:
-            res += self.nums[i]
-            i += 1
-        return res
+        if len(nums) < 1:
+            return
+        self.cnums = [0]
+        c = nums[0]
+        self.cnums.append(c)
+        for idx in range(1, len(nums)):
+            c += nums[idx]
+            self.cnums.append(c)
 
     def sumRange(self, i, j):
         """
@@ -25,9 +20,14 @@ class NumArray(object):
         :type j: int
         :rtype: int
         """
-
+        return self.cnums[j+1] - self.cnums[i]
         
-
+if __name__ == '__main__':
+    data = [-2,0,3,-5,2,-1]
+    s = NumArray(data)
+    #print s.sumRange(0,2)
+    print s.sumRange(2,5)
+    print s.sumRange(0,5)
 
 # Your NumArray object will be instantiated and called as such:
 # numArray = NumArray(nums)
