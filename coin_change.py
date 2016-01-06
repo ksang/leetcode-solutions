f24c810feac485a85c27e7a810f55b28ac57b15e
ksang <kaitoy@qq.com>
2016-01-06 15:19:55 +0800

    coin range dev

--- coin_change.py
+++ coin_change.py
@@ -1,4 +1,17 @@
 class Solution(object):
+
+    def coin_calc(self, coins, amount, ccount, pos):
+        # there is no combination
+        if pos < 0:
+            return -1
+        if coins[pos] > amount:
+            self.coin_calc(coins, amount, ccount, pos-1)
+        else:
+            if amount % coins[pos] == 0:
+                return ccount + (amount/coins[pos])
+            else:
+
+
     def coinChange(self, coins, amount):
         """
         :type coins: List[int]
@@ -6,4 +19,5 @@ class Solution(object):
         :rtype: int
         """
         coins.sort()
+        return coin_calc(coins, amount, 0, len(coins)-1)
         
