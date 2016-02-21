c072571b7fb9fe3d67b0580666c5e7dae5f99f36
ksang <kaitoy@qq.com>
2016-02-21 23:19:26 +0800

    coin change

--- coin_change.py
+++ coin_change.py
@@ -3,13 +3,16 @@ class Solution(object):
     def coin_calc(self, coins, amount, ccount, pos):
         # there is no combination
         if pos < 0:
-            return -1
+            return
         if coins[pos] > amount:
             self.coin_calc(coins, amount, ccount, pos-1)
         else:
             if amount % coins[pos] == 0:
-                return ccount + (amount/coins[pos])
+                self.res = min(self.res, ccount + (amount/coins[pos]))
             else:
+                print "a:%s pos:%s ccount:%s" % (amount, pos, ccount)
+                self.coin_calc(coins, amount%coins[pos], ccount+(amount/coins[pos]), pos-1)
+                self.coin_calc(coins, amount, ccount, pos-1)
 
 
     def coinChange(self, coins, amount):
@@ -18,6 +21,17 @@ class Solution(object):
         :type amount: int
         :rtype: int
         """
+        if amount <= 0:
+            return 0
         coins.sort()
-        return coin_calc(coins, amount, 0, len(coins)-1)
+        self.res = 2**31-1
+        self.coin_calc(coins, amount, 0, len(coins)-1)
+        if self.res == 2**31-1:
+            return -1
+        else:
+            return self.res
+
+if __name__ == '__main__':
+    coins = [186,419,83,408]
+    print Solution().coinChange(coins, 6249)
         
