0447bdd8b75b683e23a767137f910471fa933565
ksang <kaitoy@qq.com>
2016-03-04 10:37:15 +0800

    serialization done

--- verify_preorder_serialization_of_a_binary_tree.py
+++ verify_preorder_serialization_of_a_binary_tree.py
@@ -1,28 +1,31 @@
 class Solution(object):
-    def isValidSerialization(self, preorder):
-        """
-        :type preorder: str
-        :rtype: bool
-        """
-        nodes = preorder.split(",")
-        if len(nodes) % 2 == 0:
-        	return False
-        if nodes[-1] != "#":
-        	return False
-        if nodes[0] == "#":
-            return False
-        stack = [nodes[0]]
-        for i in range(1, len(nodes)-1):
-        	if nodes[i] == "#":
-        		if stack:
-        			stack.pop(-1)
-        		else:
-        			return False
-        	else:
-        		stack.append(nodes[i])
-        if stack:
-        	return False
-        return True
+	def isValidSerialization(self, preorder):
+		"""
+		:type preorder: str
+		:rtype: bool
+		"""
+		nodes = preorder.split(",")
+		if len(nodes) % 2 == 0:
+			return False
+		if nodes[-1] != "#":
+			return False
+		if nodes[0] == "#":
+			if len(nodes) == 1:
+				return True
+			else:
+				return False
+		stack = [nodes[0]]
+		for i in range(1, len(nodes)-1):
+			if nodes[i] == "#":
+				if stack:
+					stack.pop(-1)
+				else:
+					return False
+			else:
+				stack.append(nodes[i])
+		if stack:
+			return False
+		return True
 
 if __name__ == '__main__':
-	print Solution().isValidSerialization("9,3,4,#,#,1,#,#,2,#,6,#,#")
+	print Solution().isValidSerialization("#")
